873b236e8e3f95a97f6643505b4f2beb4d8b8ec0
harshitrnjn <harshiranjan0000@gmail.com>
2025-11-30 15:10:00 +0530

    Implement GET route to fetch interested leads with user authentication and structured API responses

---
+++ src/app/api/v1/call/get-interested/route.ts
@@ -0,0 +1,46 @@
+import { dbConnect } from "@/db/dbConnect";
+import Call from "@/models/call.models";
+import { getDataToken } from "@/utils/getDataToken";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function GET(request: NextRequest){
+
+    const userId = await getDataToken(request)
+
+    if(!userId){
+        return NextResponse.json({
+            status: false,
+            message: "Unauthorized Request"
+        }, { status: 401 })
+    }
+
+    try {
+
+        await dbConnect();
+
+        const interested = await Call.find({ interested: true }).sort({craetedAt: -1})
+
+        if(!interested){
+            return NextResponse.json({
+                status: false,
+                message: "Unable to fetch Interested Lead"
+            }, { status: 400 })
+        }
+
+        return NextResponse.json({ 
+            status: true,
+            message: "Intereted Lead fetched successfully",
+            data: interested
+         }, { status:200 })
+        
+    } catch (error:any) {
+        return NextResponse.json({
+            status: false,
+            message: "Internal Server Error",
+            error: error.message
+        }, { status: 500 })
+    }
+
+}
+
