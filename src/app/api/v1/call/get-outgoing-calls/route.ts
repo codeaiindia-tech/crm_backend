8e0d3d2134546885e300c01ef9c86020d5b97724
harshitrnjn <harshiranjan0000@gmail.com>
2025-11-30 12:38:13 +0530

    Implement GET route for fetching outgoing calls with user authentication

---
+++ src/app/api/v1/call/get-outgoing-calls/route.ts
@@ -0,0 +1,44 @@
+import { getDataToken } from "@/utils/getDataToken";
+import { dbConnect } from "@/db/dbConnect";
+import Call from "@/models/call.models";
+import { NextRequest, NextResponse } from "next/server";
+
+// route for INCOMING CALLS ONLY
+export async function GET( request : NextRequest ){
+    
+    const userId = await getDataToken(request)
+
+    if(!userId){
+        return NextResponse.json({
+            status: false,
+            message: "Unauthorized request"
+        }, { status: 401 })
+    }
+
+    try {
+        await dbConnect();
+
+        const allOutgoingCalls = await Call.find({ callType: "OUTGOING" }).sort({ createdAt: -1 })
+
+        if(!allOutgoingCalls){
+            return NextResponse.json({
+                status: false,
+                message: "Unable to fetch OUTGOING CALLS"
+            }, { status: 400 })
+        }
+
+        return NextResponse.json({
+            status: true,
+            message: "Outgoing calls fetched successfully",
+            callLog: allOutgoingCalls 
+        }, { status: 200 })
+
+    } catch (error:any) {
+        return NextResponse.json({
+            status: false,
+            message: "Internal Server Error",
+            error: error.message
+        }, { status: 500 })
+    }
+
+}
